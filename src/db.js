bbb8859ae0c3f99381425ad715f5a488957608a0
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-18 15:14:07 -0500

    terminando config y tabla user creada

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const path = require('path');
 // Import enviroment variables
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/nameDB`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/yourshoes`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -15,7 +15,7 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-fs.readFileSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, '/models'))
     .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
     .forEach(file => {
         modelDefiners.push(require(path.join(__dirname, '/models', file)))
