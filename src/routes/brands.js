ab81e03c77bee3645d4311b7870839d018ec6a9f
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-21 16:20:24 -0500

    Brand ya funciona

--- src/routes/brands.js
+++ src/routes/brands.js
@@ -8,31 +8,49 @@ const { defaults } = require("pg");
 router.get("/", async (req, res) => {
   try {
     const dbBrands = await getDbBrand();
-    
+
     if (!dbBrands.length) {
-      const request = []
+      const url = "https://api.mercadolibre.com/sites/MLA/search?category=";
+      //------------------------------TODOS LOS IDS DE LAS CATEGORIAS
+      const ids = [
+        "MLA109027&BRAND=14671",
+        "MLA109027&BRAND=14810",
+        "MLA109027&BRAND=252310",
+        "MLA109027&BRAND=124578",
+        "MLA414251&BRAND=58625",
+        "MLA416005&BRAND=130142",
+        "MLA415194&BRAND=130114",
+        "MLA414674&BRAND=1088662",
+        "MLA414610&BRAND=2658635",
+        "MLA415192&BRAND=1088662",
+        "MLA414673&BRAND=238731",
+        // 'MLA455893', 'MLA414673' //---> NO TIENEN BRAND
+      ].map((e) => url + e); // armo la url ej: "https://api.mercadolibre.com/sites/MLA/search?category=MLA109027&BRAND=14671"
+      //console.log(ids)
+      const getAllApi = await Promise.all(
+        ids.map(async (link) => {
+          return (await axios(link)).data.results;
+        })
+      );
 
-      for (let i = 0; i < 4; i++) {
-        request.push(axios(`https://api.mercadolibre.com/sites/MLA/search?q=zapatillas&offset=${i}`))
-      }
-
-      const brandsApi = (await Promise.all(request)).map(result => result.data.results).flat()
-
-      const brandsMap = brandsApi.map((s) => ({
-        id: s.attributes[0].value_id || `${Math.round(Math.random() * 1000000000)}`,
-        brand: s.attributes[0].value_name
+      const brandsApi = getAllApi.flat().map(e => ({ 
+        id: e.attributes[0].values[0].id || `${Math.round(Math.random() * 1000000000)}`, 
+        name: e.attributes[0].value_name 
       }))
+ 
+      console.log(brandsApi);
 
-      const setBrands = [...new Set(brandsMap.map(JSON.stringify))].map(e => JSON.parse(e))
+      const setBrands = [...new Set(brandsApi.map(JSON.stringify))].map(e => JSON.parse(e))
       //me la llevo para toda la vida
-     
-        setBrands.forEach(async(s)=>{
+
+
+      setBrands.forEach(async (s) => {
         await Brand.findOrCreate({
-          where: {id:s.id},
-          defaults:{ name: s.brand}
+          where: { id: s.id },
+          defaults: { name: s.brand }
         })
       })
-    
+
       res.status(200).send(setBrands)
     } else {
       res.send(dbBrands)
