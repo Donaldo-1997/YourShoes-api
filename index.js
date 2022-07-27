1eeb2c20ef861fae8c1eb758eee40e30cf8e2635
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-27 15:04:16 -0500

    force en false y cambio PORT

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false }).then(() => {
   
-  
-    server.listen(process.env.PORT, () => {
+    const PORT = process.env.PORT || 3001
+    server.listen(PORT, () => {
     console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
