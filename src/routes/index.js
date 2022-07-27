5a278d27b833aa3d26a332200ab370a79a61a7cc
Donaldo-1997 <donaldojbm7@gmail.com>
2022-07-27 14:50:06 -0500

    prueba con ruta /

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,9 @@ const login = require('./login')
 const google = require('./google')
 const mercadoRouter = require("./mercadopago.js")
 //--------------------------ROUTES
+router.use("/",(req, res) => {
+    res.send('prueba de deploy')
+})
 router.use("/google", google)
 router.use("/login", login)
 router.use("/user/", user);
